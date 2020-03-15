14ea42e0f80234219e3f0068c67bcf544891f60d
Ruslebiff <joakivb@stud.ntnu.no>
2020-03-15 02:36:45 +0100

    chromemajPF done

--- chromemajPF.bash
+++ chromemajPF.bash
@@ -1,1 +1,10 @@
 #!/bin/bash
+for pid in $(pgrep chrome); do
+  major_page_faults=$(ps --no-headers -o maj_flt $pid)
+  message="Chrome $pid har forårsaket \t $major_page_faults major page faults"
+  if [ $major_page_faults -gt 1000 ]
+  then
+    message="$message (mer enn 1000!)"
+  fi
+  echo -e $message
+done
