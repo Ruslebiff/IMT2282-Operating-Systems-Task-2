7d9d74bda1299d6b9c8ccfa9ce0a989e839d58f1
Ruslebiff <joakivb@stud.ntnu.no>
2020-03-15 01:41:56 +0100

    menu structure done

--- myprocinfo.bash
+++ myprocinfo.bash
@@ -6,14 +6,44 @@ clear
 while [ "$svar" != "9" ]
 do
  echo ""
- echo "  1 - Hvem er jeg?"
- echo "  9 - Avslutt dette scriptet"
+ echo "	1 - Hvem er jeg og hva er navnet på dette scriptet?"
+ echo "	2 - Hvor lenge er det siden siste boot?"
+ echo "	3 - Hva var gjennomsnittlig load siste minutt?"
+ echo "	4 - Hvor mange prosesser og tråder finnes?"
+ echo "	5 - Hvor mange context switch'er fant sted siste sekund?"
+ echo "	6 - Hvor mange interrupts fant sted siste sekund?"
+ echo "	9 - Avslutt dette scriptet"
  echo ""
  echo -n "Velg en funksjon: "
  read -r svar
  echo ""
  case $svar in
   1)clear
+    echo "Jeg er $(whoami)"
+    read -r
+    clear
+    ;;
+  2)clear
+    echo "Jeg er $(whoami)"
+    read -r
+    clear
+    ;;
+  3)clear
+    echo "Jeg er $(whoami)"
+    read -r
+    clear
+    ;;
+  4)clear
+    echo "Jeg er $(whoami)"
+    read -r
+    clear
+    ;;
+  5)clear
+    echo "Jeg er $(whoami)"
+    read -r
+    clear
+    ;;
+  6)clear
     echo "Jeg er $(whoami)"
     read -r
     clear
