5dda668f5b40daaeb8d00d8a8b21751d038c04ee
Ruslebiff <joakivb@stud.ntnu.no>
2020-03-15 13:50:24 +0100

    shellcheck fix

--- procmi.bash
+++ procmi.bash
@@ -11,17 +11,18 @@ do
     vmlib=$(grep VmLib "/proc/$pid/status" | awk '{print $2}')
     vmrss=$(grep VmRSS "/proc/$pid/status" | awk '{print $2}')
     vmpte=$(grep VmPTE "/proc/$pid/status" | awk '{print $2}')
-    vmdse=$(($vmdata+$vmstk+$vmexe))
+    vmdse=$((vmdata+vmstk+vmexe))
 
     date=$(date +%Y%m%d-%H:%M:%S)
 	file="$pid-$date.meminfo"
-
-	echo "******** Minneinfo om prosess med PID $pid ********" >> "$file"
-	echo "Total bruk av virtuelt minne (VmSize): $vmsize" >>"$file"
-	echo "Mengde privat virtuelt minne (VmData+VmStk+VmExe): $vmdse" >> "$file"
-	echo "Mengde shared virtuelt minne (VmLib): $vmlib" >>  "$file"
-	echo "Total bruk av fysisk minne (VmRSS): $vmrss" >>  "$file"
-	echo "Mengde fysisk minne som benyttes til page tables (VmPTE): $vmpte" >> "$file"
+  {
+	echo "******** Minneinfo om prosess med PID $pid ********"
+	echo "Total bruk av virtuelt minne (VmSize): $vmsize"
+	echo "Mengde privat virtuelt minne (VmData+VmStk+VmExe): $vmdse"
+	echo "Mengde shared virtuelt minne (VmLib): $vmlib"
+	echo "Total bruk av fysisk minne (VmRSS): $vmrss"
+	echo "Mengde fysisk minne som benyttes til page tables (VmPTE): $vmpte"
+  } >> "$file"
   else
     echo "Prosess med pid $pid finnes ikke"
   fi
